ca1ba11894ef55c9cb3791be05413f29c0d3defc
BOSS <74383033+MLBB-BOSS@users.noreply.github.com>
2025-07-15 09:06:38 +0300

    Update user_states.py

--- states/user_states.py
+++ states/user_states.py
@@ -7,10 +7,6 @@ class RegistrationFSM(StatesGroup):
     """
     Стани для покрокового процесу реєстрації та оновлення профілю користувача.
     """
-    # Початкова реєстрація
-    waiting_for_photo = State()
-    waiting_for_confirmation = State()
-    
     # Оновлення даних
     waiting_for_basic_photo = State()      # Очікування скріншота з базовою інформацією
     waiting_for_stats_photo = State()      # Очікування скріншота зі загальною статистикою
